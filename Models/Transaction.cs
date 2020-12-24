25602f982f8b579c8d929512bb0150b4db584b38
breezycloud <38924174+breezycloud@users.noreply.github.com>
2020-12-24 10:38:22 +0100

    final

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 #nullable disable
 
@@ -20,6 +21,7 @@ namespace ShowroomAPI.Models
         public int? StaffId { get; set; }
         public decimal? Discount { get; set; }
         public decimal? SubTotal { get; set; }
+        [JsonIgnore]
         public byte[] Receipt { get; set; }
 
         public virtual ICollection<TransactionDetail> TransactionDetails { get; set; }
